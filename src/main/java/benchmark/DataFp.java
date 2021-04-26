a5c09063d06b1eba58065061e0089029c5c6eb05
Ranger Shaw <rangershaw96@outlook.com>
2021-04-26 20:39:27 +0800

    init

--- src/main/java/benchmark/DataFp.java
+++ src/main/java/benchmark/DataFp.java
@@ -8,6 +8,7 @@ public class DataFp {
         3   ...
     */
 
+    // test INSERT
     static String[][] CSV_INSERT_INPUT = new String[][]{
             {
                     "dataFiles\\letter\\insert\\letter_15000.csv",
@@ -16,11 +17,15 @@ public class DataFp {
                     "dataFiles\\letter\\insert\\letter_17000-18000.csv",
                     "dataFiles\\letter\\insert\\letter_18000-19000.csv",
                     "dataFiles\\letter\\insert\\letter_19000-20000.csv"
+            },
+            {
+                    // the next dataset file path
             }
     };
 
     static String[] DIFF_INSERT_INPUT = new String[]{
             "dataFiles\\letter\\insert\\letter_DS_15000.txt"
+            // the next dataset file path
     };
 
     static String[][] FD_INSERT_OUTPUT = new String[][]{
@@ -31,7 +36,29 @@ public class DataFp {
                     "dataFiles\\letter\\insert\\letter_FD_18000.txt",
                     "dataFiles\\letter\\insert\\letter_FD_19000.txt",
                     "dataFiles\\letter\\insert\\letter_FD_20000.txt"
+            },
+            {
+                    // the next dataset file path
             }
     };
+
+
+    // test REMOVE
+    static String[][] CSV_REMOVE_INPUT = new String[][]{
+            {
+
+            }
+    };
+
+    static String[] DIFF_REMOVE_INPUT = new String[]{
+
+    };
+
+    static String[][] FD_REMOVE_OUTPUT = new String[][]{
+            {
+
+            }
+    };
+
 
 }
