d6e9b3958ca0950a3856c5225e09a645124af3da
Ranger Shaw <rangershaw96@outlook.com>
2021-07-07 22:35:59 +0800

    CAB

--- src/main/java/benchmark/Benchmark.java
+++ src/main/java/benchmark/Benchmark.java
@@ -3,15 +3,15 @@ package benchmark;
 public class Benchmark {
 
     public static void main(String[] args) {
-        int dataset = 0;
+        int dataset = 5;
 
         //TestCase.testInsert(dataset);
-        //TestCase.testRemove(dataset);
+        TestCase.testRemove(dataset);
         //TestCase.testDiff(dataset);
         //TestCase.testInsertDiff(dataset);
         //TestCase.testRemoveDiff(dataset);
 
-        TestCase.testApp(dataset, 0.001);
+        //TestCase.testApp(dataset, 0.001);
     }
 
 }
--- src/main/java/benchmark/DataFp.java
+++ src/main/java/benchmark/DataFp.java
@@ -11,6 +11,7 @@ public class DataFp {
         2   echo
         3   pitches
         4   hepatitis
+        5   cab
     */
 
 
@@ -21,7 +22,8 @@ public class DataFp {
             "dataFiles/reduced/insert/fd-reduced_200000.csv",
             "dataFiles/echo/insert/echocardiogram_70.csv",
             "",
-            "dataFiles/hepatitis/insert/hepatitis_71.csv"
+            "dataFiles/hepatitis/insert/hepatitis_71.csv",
+            "dataFiles/cab/insert/CAB_10000.csv",
     };
 
     static String[] INSERT_INPUT_BASE_DIFF = new String[]{
@@ -29,7 +31,8 @@ public class DataFp {
             "dataFiles/reduced/insert/fd-reduced_DS_200000.csv",
             "dataFiles/echo/insert/echocardiogram_DS_70.csv",
             "",
-            "dataFiles/hepatitis/insert/hepatitis_DS_71.csv"
+            "dataFiles/hepatitis/insert/hepatitis_DS_71.csv",
+            "dataFiles/cab/insert/CAB_DS_10000.csv",
     };
 
     static String[] INSERT_OUTPUT_BASE_FD = new String[]{
@@ -62,6 +65,13 @@ public class DataFp {
             },
             {
 
+            },
+            {
+                    "dataFiles/cab/insert/CAB_54_10001-11000.csv",
+                    "dataFiles/cab/insert/CAB_54_11001-12000.csv",
+                    "dataFiles/cab/insert/CAB_54_12001-13000.csv",
+                    "dataFiles/cab/insert/CAB_54_13001-14000.csv",
+                    "dataFiles/cab/insert/CAB_54_14001-15000.csv",
             }
     };
 
@@ -79,6 +89,9 @@ public class DataFp {
                     "dataFiles/reduced/insert/fd-reduced_DS_230000.csv",
                     "dataFiles/reduced/insert/fd-reduced_DS_240000.csv",
                     "dataFiles/reduced/insert/fd-reduced_DS_250000.csv",
+            },
+            {
+
             },
             {
 
@@ -115,6 +128,18 @@ public class DataFp {
             },
             {
 
+            },
+            {
+                    "dataFiles/cab/insert/CAB_FD-11000.csv",
+                    "dataFiles/cab/insert/CAB_FD-12000.csv",
+                    "dataFiles/cab/insert/CAB_FD-13000.csv",
+                    "dataFiles/cab/insert/CAB_FD-14000.csv",
+                    "dataFiles/cab/insert/CAB_FD-15000.csv",
+                    "dataFiles/cab/insert/CAB_FD-16000.csv",
+                    "dataFiles/cab/insert/CAB_FD-17000.csv",
+                    "dataFiles/cab/insert/CAB_FD-18000.csv",
+                    "dataFiles/cab/insert/CAB_FD-19000.csv",
+                    "dataFiles/cab/insert/CAB_FD-20000.csv",
             }
     };
 
@@ -127,7 +152,8 @@ public class DataFp {
             "dataFiles/reduced/remove/fd-reduced_250000.csv",
             "",
             "dataFiles/pitches/remove/2019_pitches_250000.csv",
-            "dataFiles/hepatitis/remove/hepatitis_154.csv"
+            "dataFiles/hepatitis/remove/hepatitis_154.csv",
+            "dataFiles/cab/remove/CAB_15000.csv",
     };
 
     static String[] REMOVE_INPUT_BASE_DIFF = new String[]{
@@ -135,7 +161,8 @@ public class DataFp {
             "dataFiles/reduced/remove/fd-reduced_DS_250000.csv",
             "",
             "dataFiles/pitches/remove/2019_pitches_DS_250000.txt",
-            "dataFiles/hepatitis/remove/hepatitis_DS_154.txt"
+            "dataFiles/hepatitis/remove/hepatitis_DS_154.txt",
+            "dataFiles/cab/remove/CAB_DS_15000.csv",
     };
     static String[] REMOVE_OUTPUT_BASE_FD = new String[]{
             "dataFiles/letter/remove/letter_FD_20000.txt",
@@ -165,6 +192,13 @@ public class DataFp {
             },
             {
                     "dataFiles/hepatitis/remove/hepatities_72-154.csv",
+            },
+            {
+                    "dataFiles/cab/remove/CAB_del_14000-14999.csv",
+                    "dataFiles/cab/remove/CAB_del_13000-13999.csv",
+                    "dataFiles/cab/remove/CAB_del_12000-12999.csv",
+                    "dataFiles/cab/remove/CAB_del_11000-11999.csv",
+                    "dataFiles/cab/remove/CAB_del_10000-10999.csv",
             }
     };
 
@@ -188,6 +222,16 @@ public class DataFp {
             },
             {
                     "dataFiles/pitches/remove/2019_pitches_DS_241666.csv"
+            },
+            {
+
+            },
+            {
+                    "dataFiles/cab/remove/CAB_FD_14000.csv",
+                    "dataFiles/cab/remove/CAB_FD_13000.csv",
+                    "dataFiles/cab/remove/CAB_FD_12000.csv",
+                    "dataFiles/cab/remove/CAB_FD_11000.csv",
+                    "dataFiles/cab/remove/CAB_FD_10000.csv",
             }
     };
 
@@ -243,6 +287,9 @@ public class DataFp {
                     "dataFiles/hepatitis/diff/hepatitis_154.csv",
                     "dataFiles/hepatitis/diff/hepatitis_70.csv",
                     "dataFiles/hepatitis/diff/hepatitis_72.csv",
+            },
+            {
+                    "dataFiles/cab/diff/CAB_15000.csv",
             }
     };
 
@@ -277,7 +324,11 @@ public class DataFp {
                     "dataFiles/hepatitis/diff/hepatitis_DS_154.csv",
                     "dataFiles/hepatitis/diff/hepatitis_70.csv",
                     "dataFiles/hepatitis/diff/hepatitis_72.csv",
+            },
+            {
+                    "dataFiles/cab/diff/CAB_DS_15000.csv",
             }
+
     };
 
 
